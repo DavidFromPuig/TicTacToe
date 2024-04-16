150fb9391a965fb41922b8e805130975ac45f89d
DavidFromPuig <david.estorach.perez@elpuig.xeill.net>
2024-04-16 17:54:56 +0200

    Initial commit v0.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! This is a Tic Tac Toe B)");
-        //We need to push it
+        System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n1-\n2-\n3-\n4-");
+
     }
 }
