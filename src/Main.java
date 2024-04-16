742e6291ff236f1397b0e2348512f6df22ccd270
DavidFromPuig <david.estorach.perez@elpuig.xeill.net>
2024-04-16 17:56:46 +0200

    Canvis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n1-\n2-\n3-\n4-");
-
+        System.out.println("Hola");
     }
 }
