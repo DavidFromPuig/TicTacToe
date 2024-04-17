20b4e0d63bea4dbf0dcd0d196fbabce9205834f9
DavidFromPuig <david.estorach.perez@elpuig.xeill.net>
2024-04-17 19:58:59 +0200

    Menu finished (no Class no Methods) v0.01

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("####################\n# TIC TAC TOE v0.1 #\n####################");
-        System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n- 1 New game\n- 2 Continue\n- 3 Settings\n- 4 Quit");
-        System.out.print("Your choice (1-4): ");
-        short menuChoice = sc.nextShort();
+
+        while(true) {
+            System.out.println("####################\n# TIC TAC TOE v0.1 #\n####################");
+            System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n- 1 New game\n- 2 Continue\n- 3 Settings\n- 4 Quit");
+            System.out.print("Your choice (1-4): ");
+            short  menuChoice = sc.nextShort();
+
+            if (menuChoice == 1) {
+                System.out.println("The option you have chosen is to create a new game! (" + menuChoice + ")");
+            } else if (menuChoice == 2) {
+                System.out.println("The option you have chosen is to continue a game. (" + menuChoice + ")");
+            } else if (menuChoice == 3) {
+                System.out.println("You have chosen to adjust the game. (" + menuChoice + ")");
+            } else if (menuChoice == 4) {
+                System.out.println("Noooo :( why would you do that?! Exiting...");
+            } else {
+                System.out.println("That is not a valid choice *raises one eyebrow*");
+            }
+
+        }
     }
 }
