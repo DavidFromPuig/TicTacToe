8d732cad867c309ec34e77493a045c048f278582
DavidFromPuig <david.estorach.perez@elpuig.xeill.net>
2024-04-17 19:39:06 +0200

    Menu base

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n1-\n2-\n3-\n4-");
-        System.out.println("Hola");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("####################\n# TIC TAC TOE v0.1 #\n####################");
+        System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n- 1 New game\n- 2 Continue\n- 3 Settings\n- 4 Quit");
+        System.out.print("Your choice (1-4): ");
+        short menuChoice = sc.nextShort();
     }
 }
