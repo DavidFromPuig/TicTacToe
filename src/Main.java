12f7d5cb2da71acf1bdc15833483f9288034c516
DavidFromPuig <david.estorach.perez@elpuig.xeill.net>
2024-04-17 20:16:50 +0200

    Menu refactoring (no Class, with Methods) v0.02

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        mainMenu();
+    }
+    private static void mainMenu() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("\n####################\n# TIC TAC TOE v0.1 #\n####################");
+        System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n- 1 New game\n- 2 Continue\n- 3 Settings\n- 4 Quit");
+        System.out.print("Your choice (1-4): ");
+        short  menuChoice = sc.nextShort();
 
-        while(true) {
-            System.out.println("####################\n# TIC TAC TOE v0.1 #\n####################");
-            System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n- 1 New game\n- 2 Continue\n- 3 Settings\n- 4 Quit");
-            System.out.print("Your choice (1-4): ");
-            short  menuChoice = sc.nextShort();
-
-            if (menuChoice == 1) {
-                System.out.println("The option you have chosen is to create a new game! (" + menuChoice + ")");
-            } else if (menuChoice == 2) {
-                System.out.println("The option you have chosen is to continue a game. (" + menuChoice + ")");
-            } else if (menuChoice == 3) {
-                System.out.println("You have chosen to adjust the game. (" + menuChoice + ")");
-            } else if (menuChoice == 4) {
-                System.out.println("Noooo :( why would you do that?! Exiting...");
-            } else {
-                System.out.println("That is not a valid choice *raises one eyebrow*");
-            }
-
+        if (menuChoice == 1) {
+            newGame(menuChoice);
+        } else if (menuChoice == 2) {
+            continueM(menuChoice);
+        } else if (menuChoice == 3) {
+            System.out.println("You have chosen to adjust the game. (" + menuChoice + ")");
+            mainMenu();
+        } else if (menuChoice == 4) {
+            System.out.println("Noooo :( why would you do that?! Exiting...");
+            mainMenu();
+        } else {
+            System.out.println("That is not a valid choice *raises one eyebrow* (" + menuChoice + ")");
+            mainMenu();
         }
     }
+    private static void newGame(short menuChoice){
+        System.out.println("The option you have chosen is to create a new game! (" + menuChoice + ")");
+        mainMenu();
+    }
+    private static void continueM(short menuChoice){
+        System.out.println("The option you have chosen is to continue a game. (" + menuChoice + ")");
+        mainMenu();
+    }
+    //it is needed to do the methods for settings and quit, good afternoon <3
 }
