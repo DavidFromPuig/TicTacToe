c9fa9d40e916216ffec05fd8fe326b6805df36ab
DavidFromPuig <david.estorach.perez@elpuig.xeill.net>
2024-04-22 16:42:22 +0200

    Menu refactoring (no Class, with Methods) v0.05 - menuChoice bad input handled

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,25 @@ public class Main {
         System.out.println("\n####################\n# TIC TAC TOE v0.1 #\n####################");
         System.out.println("Welcome to the Tic Tac Toe menu!\nWhat do you want to do?\n- 1 New game\n- 2 Continue\n- 3 Settings\n- 4 Quit");
         System.out.print("Your choice (1-4): ");
-        short  menuChoice = sc.nextShort();
+        try{
+            short  menuChoice = sc.nextShort();
 
-        if (menuChoice == 1) {
-            newGame(menuChoice);
-        } else if (menuChoice == 2) {
-            continueM(menuChoice);
-        } else if (menuChoice == 3) {
-            settings(menuChoice);
-        } else if (menuChoice == 4) {
-            exit();
-        } else {
-            badChoice(menuChoice);
+            if (menuChoice == 1) {
+                newGame(menuChoice);
+            } else if (menuChoice == 2) {
+                continueM(menuChoice);
+            } else if (menuChoice == 3) {
+                settings(menuChoice);
+            } else if (menuChoice == 4) {
+                exit();
+            } else {
+                badChoice();
+            }
         }
+        catch (Exception ex){
+            badChoice();
+        }
+
     }
     private static void newGame(short menuChoice){
         System.out.println("The option you have chosen is to create a new game! (" + menuChoice + ")");
@@ -40,8 +46,8 @@ public class Main {
         System.out.println("Noooo :( why would you do that?! Exiting...");
         mainMenu();
     }
-    private static void badChoice(short menuChoice){
-        System.out.println("That is not a valid choice *raises one eyebrow* (" + menuChoice + ")");
+    private static void badChoice(){
+        System.out.println("That is not a valid choice *raises one eyebrow*");
         mainMenu();
     }
 }
