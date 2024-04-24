5e8c3bf895bd7578904445d9db4bd8b344646702
MelanieBau <nivegb@elpuig.xeill.net>
2024-04-24 19:24:30 +0200

    Game class creation v0.06

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         mainMenu();
+        Joc joc = new Joc();
     }
     private static void mainMenu() {
         Scanner sc = new Scanner(System.in);
