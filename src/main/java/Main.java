1f6303fbdc35dd0c5e682a9b35e92a935b0c7f5e
DavidFromPuig <david.estorach.perez@elpuig.xeill.net>
2024-05-08 19:33:11 +0200

    Menu fixing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,20 +7,23 @@ public class Main {
 
         menu:
         while (true) {
-            short opt = tui.showMenu();
+            short option = tui.showMenu();
 
-            switch (opt) {
+            switch (option) {
                 case 1:
                     newGameOption(game, tui);
                     break;
 
                 case 2:
+                    loadGame(game, tui);
                     break;
 
                 case 3:
+                    settings(game, tui);
                     break;
 
                 case 4:
+                    exit(tui);
                     break menu;
             }
         }
@@ -34,20 +37,16 @@ public class Main {
         //recollir jugada (bucle a dins)
 
     }
-    public static void loadGame(short menuChoice){
-        System.out.println("The option you have chosen is to continue a game. (" + menuChoice + ")");
-        TUI.showMenu();
+    public static void loadGame(Game game, TUI tui){
+        //System.out.println("The option you have chosen is to continue a game. (" + menuChoice + ")");
     }
-    public static void settings(short menuChoice){
-        System.out.println("You have chosen to adjust the game. (" + menuChoice + ")");
-        TUI.showMenu();
+    public static void settings(Game game, TUI tui){
+        //System.out.println("You have chosen to adjust the game. (" + menuChoice + ")");
     }
-    public static void exit(short menuChoice){
-        System.out.println("Noooo :( why would you do that?! Exiting...");
-        TUI.showMenu();
+    public static void exit(TUI tui){
+        //System.out.println("Noooo :( why would you do that?! Exiting...");
     }
-    public static void badChoice(short menuChoise){
-        System.out.println("That is not a valid choice *raises one eyebrow*");
-        TUI.showMenu();
+    public static void badChoice(){
+        //System.out.println("That is not a valid choice *raises one eyebrow*");
     }
 }
